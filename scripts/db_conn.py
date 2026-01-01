d4cedc6ff3921795db6e3762478d4f75234f6a5d
Human-Gechi <kimberlydaniel363@gmail.com>
2026-01-01 23:15:07 +0100

    Doctstrings and comments
    Added docstrings to functions for better documentation and clarity. Updated error handling to return an empty list if no connection is made.

--- scripts/db_conn.py
+++ scripts/db_conn.py
@@ -1,3 +1,4 @@
+#Necessary Libraries for the task
 import os
 from dotenv import load_dotenv
 from airflow_task.scripts.logs import logger
@@ -5,9 +6,10 @@ import snowflake.connector
 
 load_dotenv()
 
-# Keep this only for the 'analysis' task which returns data to Python
 def get_snowflake_connection():
+    """Function to make a Snowflake Connection"""
     try:
+        #Making Snowflake connection.
         return snowflake.connector.connect(
             user=os.getenv("SNOWFLAKE_USER"),
             password=os.getenv("SNOWFLAKE_PASSWORD"),
@@ -18,12 +20,15 @@ def get_snowflake_connection():
             role=os.getenv("SNOWFLAKE_ROLE")
         )
     except Exception as e:
+        #Error message
         logger.error(f"Could not connect to Snowflake: {e}")
         return None
 
 def get_setup_sql():
     """SQL to initialize all required objects."""
-    return """
+    return 
+    #Create staging and final tables
+    """
         CREATE TABLE IF NOT EXISTS WIKI_PAGES_VIEWS_STAGING (
             DOMAIN_CODE STRING,
             PAGE_TITLE STRING,
@@ -41,7 +46,7 @@ def get_setup_sql():
     """
 
 def get_copy_sql():
-    """SQL for high-performance loading."""
+    """SQL for high-performance loading instead of loading with_pandas to prevent signal_9 errors in Airflow"""
     return """
         COPY INTO WIKI_PAGES_VIEWS_STAGING
         FROM @WIKI_STAGING_STAGE
@@ -56,6 +61,7 @@ def get_copy_sql():
 
 def get_production_insert_sql():
     """SQL for moving data to the final layer."""
+    # Function to select just required companies e.g Amazon, Microsoft,Apple etc
     return """
         INSERT INTO WIKI_PAGES_VIEWS_FINAL (DOMAIN_CODE, PAGE_TITLE, VIEW_COUNT, RESPONSE_IN_BYTES)
         SELECT DOMAIN_CODE, PAGE_TITLE, VIEW_COUNT, RESPONSE_IN_BYTES
@@ -64,11 +70,14 @@ def get_production_insert_sql():
     """
 
 def select_companies_to_list():
+    """SQL for selecting company analysis"""
     conn = get_snowflake_connection()
-    if not conn: return []
+    if not conn: 
+        return [] #Return empty list of no connection was made
     try:
         with conn.cursor() as cur:
             cur.execute("SELECT PAGE_TITLE, SUM(VIEW_COUNT) FROM WIKI_PAGES_VIEWS_FINAL GROUP BY 1;")
+           #Output message for companies in the group 
             return [{"company": r[0], "total_views": r[1]} for r in cur.fetchall()]
     finally:
         conn.close()
