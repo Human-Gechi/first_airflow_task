9e4760ebdbe3c99bd710400310b083f453b3601f
Human-Gechi <kimberlydaniel363@gmail.com>
2026-01-03 02:02:07 +0100

    Code

--- dags_folder/dags.py
+++ dags_folder/dags.py
@@ -1,3 +1,4 @@
+#Importing necessary libraries
 import os
 import sys
 from pendulum import datetime
@@ -9,11 +10,12 @@ from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.utils.trigger_rule import TriggerRule
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+#Acessing file path for .py files
 from airflow_task.scripts.db_conn import (
     get_setup_sql, get_copy_sql,
-    get_production_insert_sql
+    get_production_insert_sql, select_companies_to_list
 )
-
+#Utilizing the domain dag for wikipedia
 @dag(
     dag_id='wikipedia_dag',
     start_date=datetime(2025, 12, 30),
@@ -22,17 +24,20 @@ from airflow_task.scripts.db_conn import (
     tags=['snowflake', 'optimized']
 )
 def wikipedia_pipeline():
+    """ Getting the function running """
 
-    @task.branch(task_id="check_file_exists")
-    def check_ext_task(folder_path):
-        folder = Path(folder_path)
+    @task.branch(task_id="check_file_exists") #Using TaskFlow API alongside its decorator
+    def check_ext_task(folder_path): #Checking file path
+        folder = Path(folder_path) #Folder path
         return "setup_snowflake" if list(folder.glob("*.gz")) else "download_file"
 
+    #BashOperator for file download
     download_file = BashOperator(
         task_id="download_file",
         bash_command="bash /opt/airflow/dags/airflow_task/download/download.sh"
     )
 
+    #Setting up Snowflake using the get_setup_sql func.
     setup_snowflake = SQLExecuteQueryOperator(
         task_id="setup_snowflake",
         conn_id='snowflake_hook',
@@ -40,43 +45,48 @@ def wikipedia_pipeline():
         trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
     )
 
+    #Parsing file from .gz into temp. storage
     @task(task_id="parse_file_to_csv")
     def parse_to_csv():
+        """ CSV parsinf for temporary storage"""
+        #File importation inside function to prevent dag failures
         from airflow_task.scripts.file_parser import dataframe_parser
         return dataframe_parser()
 
+    #Uploading file to staging table
     @task(task_id="upload_to_stage")
     def upload_to_stage(local_csv_path):
         hook = SnowflakeHook(snowflake_conn_id='snowflake_hook')
         hook.run(f"PUT file://{local_csv_path} @WIKI_STAGING_STAGE OVERWRITE=TRUE")
 
-
+    #COPYING data into staging
     copy_to_staging = SQLExecuteQueryOperator(
         task_id="copy_into_staging",
         conn_id='snowflake_hook',
         sql=get_copy_sql()
     )
 
-
+    #Inserting into production table
     insert_prod = SQLExecuteQueryOperator(
         task_id="insert_into_production",
         conn_id='snowflake_hook',
         sql=get_production_insert_sql()
     )
 
-
+    #Companies analysis
     analyze_data = SQLExecuteQueryOperator(
         task_id="companies_analysis",
         conn_id='snowflake_hook',
-        sql="SELECT PAGE_TITLE, SUM(VIEW_COUNT) FROM WIKI_PAGES_VIEWS_FINAL GROUP BY 1;",
+        sql=select_companies_to_list,
         do_xcom_push=True
     )
-
+    #CHecking file path
     path_decision = check_ext_task("/opt/airflow/dags/airflow_task")
-    csv_path = parse_to_csv()
+    csv_path = parse_to_csv() #CSV file path
 
-    path_decision >> [download_file, setup_snowflake]
-    download_file >> setup_snowflake
-    setup_snowflake >> csv_path >> upload_to_stage(csv_path) >> copy_to_staging >> insert_prod >> analyze_data
+    #Defining dependencies
+    path_decision >> [download_file, setup_snowflake] #Checks existence of file and downloads then setup
+    download_file >> setup_snowflake #File is available, setup Snowflake
+    setup_snowflake >> csv_path >> upload_to_stage(csv_path) >> copy_to_staging >> insert_prod >> analyze_data  #Setup Snowflake,upload data
 
-wikipedia_pipeline()
+wikipedia_pipeline() #Function fot the decorator
--- scripts/file_parser.py
+++ scripts/file_parser.py
@@ -4,36 +4,38 @@ from pathlib import Path
 from airflow_task.scripts.logs import logger
 
 def get_gz_file(folder_path):
+    """ Gets .gz file path"""
     folder = Path(folder_path)
     gz_files = list(folder.glob("*.gz"))
     return gz_files[0] if gz_files else None
 
 def dataframe_parser():
     """Parses .gz and writes a clean CSV for Snowflake COPY INTO."""
-    input_path = get_gz_file("/opt/airflow/dags/airflow_task")
-    output_path = "/tmp/processed_wiki_data.csv"
+    input_path = get_gz_file("/opt/airflow/dags/airflow_task") #folder path for .gz file
+    output_path = "/tmp/processed_wiki_data.csv" #temporary storage of csv file
 
     if not input_path:
-        logger.error("No input file found.")
+        logger.error("No input file found.") #Error message id no file found
         return None
 
-    logger.info(f"Parsing {input_path} into {output_path}")
+    logger.info(f"Parsing {input_path} into {output_path}") #Info: Parsing .gz into the temp. storage
 
-    with gzip.open(input_path, "rt", encoding="utf-8", errors="replace") as f_in:
-        with open(output_path, "w", newline="", encoding="utf-8") as f_out:
-            writer = csv.writer(f_out, delimiter='|')
+    with gzip.open(input_path, "rt", encoding="utf-8", errors="replace") as f_in: #Reading .gz file as f_in
+        with open(output_path, "w", newline="", encoding="utf-8") as f_out: #Opening file in the output path as f_out to write as CSV
+            writer = csv.writer(f_out, delimiter='|') #CSV writer
 
-            for line in f_in:
-                parts = line.split()
-                if len(parts) < 4: continue
-                try:
-                    
-                    domain = parts[0].replace('""', "")
-                    title = " ".join(parts[1:-2])
-                    views = int(parts[-2])
-                    bytes_size = int(parts[-1])
-                    writer.writerow([domain, title, views, bytes_size])
-                except (ValueError, IndexError):
+            for line in f_in: #Loop in input path/ file
+                parts = line.split() #Splitting data on space
+                if len(parts) < 4: #Lenght of splitted parts = 3
+                    continue
+                try: #Try, Except , Block
+
+                    domain = parts[0].replace('""', "") #First part before space
+                    title = " ".join(parts[1:-2]) #Accessing the title using negative and positive indexing to avoid errors
+                    views = int(parts[-2]) #Negative indexing accessing second to the last data item
+                    bytes_size = int(parts[-1]) #Negative indexing accessing last item
+                    writer.writerow([domain, title, views, bytes_size]) #Write each row to output file in the output path
+                except (ValueError, IndexError): #Do nothing if an error occured
                     continue
 
-    return output_path
+    return output_path #Return output path
